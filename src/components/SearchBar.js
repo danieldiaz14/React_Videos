f89b504abe81031becef8985ebedb0c669088846
Daniel Diaz Medrano <danieldiaz129@gmail.com>
2018-12-02 09:48:39 -0800

    Api requests working

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,7 +11,7 @@ class SearchBar extends React.Component {
 
     onFormSubmit = (e) => {
         e.preventDefault();
-        // TODO: Make sure to make call back from parent component
+        this.props.onFormSubmit(this.state.term);
     }
     render() {
         return (
