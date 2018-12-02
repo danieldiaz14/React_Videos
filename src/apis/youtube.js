179c3006a69291a76dbc2d257faf85eb0295f6c3
Daniel Diaz Medrano <danieldiaz129@gmail.com>
2018-12-02 09:35:04 -0800

    API Config Complete

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,11 @@
+import axios from 'axios';
 const KEY = 'AIzaSyAmdrE2loQ8VMgKh9LWAD2imSBE1jFvSVg';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+    }
+});
