04a33b1ec2b80db11c420d4ab34a83c076d24801
Daniel Medrano <daniel.medrano@ASLEEPCOUCHSKI.workdayinternal.com>
2018-12-03 12:03:57 -0800

    VideoDetail component now displaying a video player

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -3,11 +3,20 @@ import React from 'react';
 
 const VideoDetail = ({video}) => {
     if (!video) {
-        return <div>Loading...</div>
+        return <div>Pick a video!</div>
     }
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
     return (
-        <div>{video.snippet.title}</div>
-    )
+        <div>
+            <div className="ui embed">
+                <iframe src={videoSrc} />
+            </div>
+            <div className="ui segment">
+                <h4 className="ui header">{video.snippet.title}</h4>
+                <p>{video.snippet.description}</p>
+            </div>
+        </div>
+    );
 };
 
 export default VideoDetail;
