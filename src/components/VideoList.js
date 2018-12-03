7aac02466de9526eb68d6b3aaadf620b5a43d1d8
Daniel Medrano <daniel.medrano@ASLEEPCOUCHSKI.workdayinternal.com>
2018-12-03 09:57:56 -0800

    Basic stlying to videoitem component

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,9 +3,9 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({videos}) => {
     const renderedList= videos.map((video) => {
-        return <VideoItem video={video}/>;
+        return <VideoItem key={video.id.videoId} video={video}/>;
     });
-    return <div>{renderedList}</div>
+    return <div className="ui relaxed divided list">{renderedList}</div>
 };
 
 export default VideoList;
