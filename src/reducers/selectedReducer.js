ce29b535355a8c07aab66eb8d069cf67a0ffd482
Daniel Diaz Medrano <danieldiaz129@gmail.com>
2019-05-20 19:48:18 -0700

    Fixed export bug

--- src/reducers/selectedReducer.js
+++ src/reducers/selectedReducer.js
@@ -1,10 +1,8 @@
-import {FETCH_DEFAULT,GO_HOME} from './../actions/types';
+import {FETCH_DEFAULT} from './../actions/types';
 
 export default (state = [], action) => {
     switch (action.type) {
         case FETCH_DEFAULT:
-            return {...state, selectedVideo: action.payload}
-        case GO_HOME:
             return {...state, selectedVideo: action.payload}
         default:
             return state;
